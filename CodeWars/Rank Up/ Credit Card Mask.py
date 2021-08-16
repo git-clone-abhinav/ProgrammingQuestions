9f71afe5a54b3dd52ded64bb7689cc8d2eade7ea
RaspberryPi <abhi.anu003@gmail.com>
2021-08-16 23:22:10 +0530

    Sum of Digits Problem

--- CodeWars/Rank Up/ Credit Card Mask.py
+++ CodeWars/Rank Up/ Credit Card Mask.py
@@ -1,7 +1,11 @@
 # https://www.codewars.com/kata/5412509bd436bd33920011bc/train/python
 
 def maskify(cc):
-    hash_length = len(cc[:-4])
-    temp = hash_length*"#"
-    ans = temp + cc[-4:]
+    t = len(cc[:-4]) # calculating the lenth for the # string
+    temp = t*"#" # creating a temporary hash string
+    ans = temp + cc[-4:] # creating the answer string
     return ans
+
+def maskify(cc):
+    return "#" * len(cc[:-4]) + cc[-4:]
+
---
+++ CodeWars/Rank Up/ Sum of Digits Digital Root.py
@@ -0,0 +1,22 @@
+def digital_root(n):
+    if len(str(n))==1: # checking for len==1 int
+           return n
+    else:
+            sum = 0 # temporary variable sum
+            for i in str(n): # iteration
+                sum = sum + int(i) # sumvalue increment
+            if len(str(sum)) > 1: # checking if lenght of string of sum is still > 1
+                return (digital_root(sum)) # if yes -> recurssion
+            else:
+                return sum # if no -> returning the value
+
+def digital_root(n):
+    return n%9 or n and 9 
+
+def digital_root(n):
+    return 1 + ((int(n) - 1) % 9) if n>0 else 0
+
+def digital_root(n):
+    while (n > 9):
+        n = reduce(lambda a, b: int(a) + int(b), list(str(n)))
+    return n
