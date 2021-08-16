a382c478ea050210cabad71ebfbeb94ec8e10db7
RaspberryPi <abhi.anu003@gmail.com>
2021-08-16 22:48:18 +0530

    corrected camel case question

--- CodeWars/Rank Up/ Convert string to camel case.py
+++ CodeWars/Rank Up/ Convert string to camel case.py
@@ -1,16 +1,22 @@
 # https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
 
-def to_camel_case(text):
-    if text == "":
+def to_camel_case(s):
+    if s == "":
         return ""
     else:
-        if "-" in text:
-            text = text.strip().split("-")
-        if " " in text:
-            text = text.strip().split(" ")
-        if "_" in text:
-            text = text.strip().split("_")
-        sent = text[0]
-        for word in text[1:]:
-            sent = sent + word.capitalize()
-        return sent
+        li = []
+        temp = ""
+
+        for i in range(len(s)):
+            if s[i]!= "-" and s[i]!= "_":
+                temp += s[i]
+            else:
+                li.append(temp)
+                temp = ""
+
+        if temp != "":
+            li.append(temp)
+        ans = li[0]
+        for i in li[1:]:
+            ans += i.capitalize()
+        return ans
